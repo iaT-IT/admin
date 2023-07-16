0899e90a0678f0b58d6582db287c77f098573b3d
bmo4401 <haiminh612a@gmail.com>
2023-07-16 20:26:13 +0700

    admin

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,14 +1,9 @@
 'use client';
 
 import { OrderColumn, columns } from './columns';
-import { ApiList } from '@/components/ui/api-list';
-import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { Billboard } from '@prisma/client';
-import { Plus } from 'lucide-react';
-import { useParams, useRouter } from 'next/navigation';
 
 interface OrderClientProps {
    data: OrderColumn[];
--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { CellAction } from './cell-action';
 import { ColumnDef } from '@tanstack/react-table';
 
 // This type is used to define the shape of our data.
